706cf7d69507649804350914d07f19ed28c815b4
Gabriel Olmos Gil <gabrielolmgil@campus.monlau.com>
2025-02-28 19:15:17 +0100

    HEADER Realizado

--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { InicioLandingComponent } from './inicio-landing/inicio-landing.component';
+import { CarruselLandingComponent } from './carrusel-landing/carrusel-landing.component';
+import { FooterLandingComponent } from './footer-landing/footer-landing.component';
 
 @Component({
   selector: 'app-landing-page',
-  imports: [],
+  imports: [InicioLandingComponent, CarruselLandingComponent, FooterLandingComponent],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.css'
 })
