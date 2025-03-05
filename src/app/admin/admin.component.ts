04cd4f72b0a5a65f2a73562670ca019f9f467151
unknown <gabrielolmgil@campus.monlau.com>
2025-03-05 13:03:26 +0100

    comienzo funcionalidades

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,24 +1,30 @@
 import { Component } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
-
+import { JsonPipe } from '@angular/common';
+import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-admin',
-  imports: [],
+  imports: [ReactiveFormsModule, JsonPipe],
   templateUrl: './admin.component.html',
   styleUrl: './admin.component.css'
 })
-export interface product{
-  refernce: string,
-  name: string,
-  price: number
-}
-export class AdminComponent {
-    productForm = new FormGroup({
-      reference: new FormControl('', [
-        Validators.required,
-        Validators.minLength(3),
-        Validators.maxLength(50),
-      ]),
 
-    });
+export class AdminComponent {
+  productForm = new FormGroup({
+    reference: new FormControl('', [
+      Validators.required,
+      Validators.minLength(3),
+      Validators.maxLength(50),
+    ]),
+    name: new FormControl(''),
+    price: new FormControl(''),
+    description: new FormControl(''),
+    check: new FormControl(''),
+    type: new FormControl(''),
+    img: new FormControl(''),
+  });
+  handleSubmit(){
+    console.log(this.productForm.value);
 }
+
+}
+
