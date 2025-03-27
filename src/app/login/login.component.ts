dd01775487bb5dfd67876ac6c6b22284a08d2125
Gabriel Olmos Gil <gabrielolmgil@campus.monlau.com>
2025-03-27 19:27:35 +0100

    login intento

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,47 +1,11 @@
 import { Component } from '@angular/core';
-import { JsonPipe } from '@angular/common';
-import { FormGroup, FormControl, Validators, ReactiveFormsModule } from '@angular/forms';
-import { ProductsService } from '../services/products.service';
+
 @Component({
-  selector: 'app-admin',
-  imports: [ReactiveFormsModule, JsonPipe],
-  templateUrl: './admin.component.html',
-  styleUrl: './admin.component.css'
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
 })
-
-export class AdminComponent {
-  constructor(private productService:ProductsService){}
-  productForm = new FormGroup({
-    reference: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(50),
-    ]),
-    name: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(50),
-    ]),
-    price: new FormControl('', [
-      Validators.required,
-      Validators.minLength(3),
-      Validators.maxLength(50),
-    ]),
-    description: new FormControl('', [
-      Validators.required,
-      Validators.minLength(10),
-      Validators.maxLength(50),
-    ]),
-    check: new FormControl(''),
-    type: new FormControl('',[
-      Validators.required,
-    ]),
-    img: new FormControl(''),
-  });
-  handleSubmit(){
-
-    console.log(this.productService.getProducts());
-}
+export class LoginComponent {
 
 }
-
